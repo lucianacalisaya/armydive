fb56bf9e5d5e69fdd0f715c614fbe578fe1cc9d7
lucianacalisaya <lulacalisaya@gmail.com>
2022-07-16 19:24:15 -0300

    Inicio del proyecto de Coderhouse

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Proyecto de React.js
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.coderhouse.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Curso Coderhouse
         </a>
       </header>
     </div>
