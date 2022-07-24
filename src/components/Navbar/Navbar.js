719fb21b8f01324cc3d9d6fdffb5f4d38e7d7257
lucianacalisaya <lulacalisaya@gmail.com>
2022-07-23 23:20:16 -0300

    Brandname

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,12 +3,13 @@ import "./Navbar.css"
 const Navbar = () => {
     return (
         <nav className="menu-container">
+            <h1><a href="index.html">Armydive</a></h1>
             <ul className="menu-list">
-                <li className="menu-list-item"><a className="menu-list-item-link" href="index.html">Inicio</a></li>
-                <li className="menu-list-item"><a className="menu-list-item-link" href="index.html">Recomendados</a></li>
-                <li className="menu-list-item"><a className="menu-list-item-link" href="index.html">Guardados</a></li>
-                <li className="menu-list-item"><a className="menu-list-item-link" href="index.html">Inicio</a></li>
-                <li className="menu-list-item"><a className="menu-list-item-link" href="index.html">Inicio</a></li>
+                <li className="menu-list-item"><a className="menu-list-item-link" href="index.html">Home</a></li>
+                <li className="menu-list-item"><a className="menu-list-item-link" href=".index.html#recomendados">Albums</a></li>
+                <li className="menu-list-item"><a className="menu-list-item-link" href="index.html#guardados">Merch</a></li>
+                <li className="menu-list-item"><a className="menu-list-item-link" href="index.html#guardados">Oficial BT21</a></li>
+                <li className="menu-list-item"><a className="menu-list-item-link" href="index.html#guardados">Hot Sale 🔥</a></li>
             </ul>
         </nav>
     )
