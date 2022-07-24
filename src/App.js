d633f3583539532906ba97f9611542bb1564b650
lucianacalisaya <lulacalisaya@gmail.com>
2022-07-23 23:26:56 -0300

    Css al navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
 
@@ -6,19 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Navbar />
-        <p>
-          Proyecto de React.js
-        </p>
-        <a
-          className="App-link"
-          href="https://www.coderhouse.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Curso Coderhouse
-        </a>
       </header>
     </div>
   );
