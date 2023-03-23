2f3aaa62a4be355acd0b4b3ca66a8915263a4b5e
lucianacalisaya <lulacalisaya@gmail.com>
2023-03-23 17:13:12 +0100

    url updated

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -27,11 +27,11 @@ const Navbar = () => {
     return (
         <nav className="menu-container">
             <ul className="menu-list">
-                <li className="menu-list-item"><Link className="menu-list-item-link" to="/">Home</Link></li>
-                <li className="menu-list-item"><Link to='/category/album' className="menu-list-item-link">Albums</Link></li>
-                <li className="menu-list-item"><Link to='/category/merch' className="menu-list-item-link">Merch</Link></li>
-                <li className="menu-list-item"><Link to='/category/bt21' className="menu-list-item-link">Oficial BT21</Link></li>
-                <li className="menu-list-item"><Link className="menu-list-item-link" to="/category/hot-sale">Hot Sale 🔥</Link></li>
+                <li className="menu-list-item"><Link className="menu-list-item-link" to="./">Home</Link></li>
+                <li className="menu-list-item"><Link to='./category/album' className="menu-list-item-link">Albums</Link></li>
+                <li className="menu-list-item"><Link to='./category/merch' className="menu-list-item-link">Merch</Link></li>
+                <li className="menu-list-item"><Link to='./category/bt21' className="menu-list-item-link">Oficial BT21</Link></li>
+                <li className="menu-list-item"><Link className="menu-list-item-link" to="./category/hot-sale">Hot Sale 🔥</Link></li>
             </ul>
         </nav>
     )
